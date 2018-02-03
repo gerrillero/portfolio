5221a771a2fae11272fcee87f6b45246214783b8
Antonio <antonio75@gmail.com>
2018-02-03 23:26:24 +0100

    search logic added

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
 })
 export class SearchComponent {
+  term: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public _ps: ProductosService) {
+    route.params.subscribe(params => {
+        this.term = params['term'];
+        // console.log(this.term);
+        _ps.search_product(this.term);
+    });
+   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,32 +1,63 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http' ;
+import { Promise, resolve, reject } from 'q';
 
 @Injectable()
 export class ProductosService {
   productos: any[] = [];
+  productos_filtrados: any[] = [];
   cargando: boolean;
 
   constructor(private http: Http) {
     this.load_productos();
    }
 
-  load_product(cod: any): any {
+  public search_product(term: string) {
+
+    if (this.productos.length === 0) {
+      this.load_productos().then( () => {
+        this.filter_products(term);
+      });
+    } else {
+      this.filter_products(term);
+    }
+  }
+
+  private filter_products(term: string) {
+    this.productos_filtrados = [];
+    term = term.toLowerCase();
+
+    this.productos.forEach(prod => {
+      if (prod.categoria.toLowerCase().indexOf(term) >= 0 || prod.titulo.toLowerCase().indexOf(term) >= 0) {
+        this.productos_filtrados.push(prod);
+      }
+    });
+  }
+
+  public load_product(cod: any): any {
     return this.http.get(`https://portfolio-26b5e.firebaseio.com/productos/${cod}.json`);
   }
 
-  load_productos(): any {
+  public load_productos() {
     this.cargando = true;
 
-    this.http.get('https://portfolio-26b5e.firebaseio.com/productos_idx.json')
+    let promise = new Promise ( (resolve, reject) =>{
+
+      this.http.get('https://portfolio-26b5e.firebaseio.com/productos_idx.json')
       .subscribe(response => {
         // console.log(response);
 
-        //setTimeout(() => {
+        // setTimeout(() => {
           this.productos = response.json();
           this.cargando = false;
        // }, 2000);
+       resolve();
 
       });
+
+      return promise;
+    });
+
   }
 
 }
